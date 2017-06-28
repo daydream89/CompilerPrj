ce30301d1a3c08a1beeb989a2083738353da7ad1
gyungmin <wnop123@gmail.com>
2017-06-28 16:12:23 +0900

    모든 기능 구현 완료(Project 4 완성)

--- test2.c
+++ test2.c
@@ -1,64 +1,29 @@
-void heapify(int arr[],int n,int i){
-	int largest;
-	int l;
-	int r;
-
-	largest = i;
-	l = 2*i + 1;
-	r = 2*i + 2;
-
-	if(l < n)
-		if(arr[l] > arr[largest])
-			largest = l;
-
-	if(r < n)
-		if(arr[r] > arr[largest])
-			largest = r;
-
-	if(largest != i){
-		int temp;
-		temp = arr[i];
-		arr[i] = arr[largest];
-		arr[largest] = temp;
-
-		heapify(arr, n, largest);
-	}
-}
-
-void heapSort(int arr[], int n){
+int findIndex(int arr[],int size,int val){
 	int i;
-	i = n/2 - 1;
-	while(i >= 0){
-		heapify(arr, n, i);
-		i = i - 1;
+
+	i = 0;
+	while(i < size){
+		if(arr[i] == val){
+			return i;
+		}
+		i = i + 1;
 	}
 
-	i = n-1;
-	while(i >= 0){
-		int temp;
-		temp = arr[0];
-		arr[0] = arr[i];
-		arr[i] = temp;
-
-		heapify(arr, i, 0);
-
-		i = i - 1;
-	}
+	return size;
 }
 
 void main(void){
 	int array[10];
 	int i;
+	int val;
 
+	i = 0;
 	while(i < 10){
 		array[i] = input();
 		i = i + 1;
 	}
+	
+	val = input();
 
-	heapSort(array,10);
-
-	while(i < 10){
-		output(array[i]);
-		i = i + 1;
-	}
+	output(findIndex(array,10,val));
 }
